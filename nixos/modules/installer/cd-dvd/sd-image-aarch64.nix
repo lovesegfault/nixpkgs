ee5ceca0b03072b0eb95a5216b5e4b0ffd04daf1
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-09-13 23:26:21 -0400

    linux: configure aarch64 contiguous memory allocator via kernel config
    As per the in-line comment, this is where distros should configure it.
    Not via kernel command line parameters.
    
    As found by looking at the implementation, while exploring the cause of
    a bug on the Raspberry Pi 4, it was found that `cma=` on the command
    line parameters will overwrite the values a device tree will have
    configured for a given platform.
    
    With this, the more recent 5.4 vendor kernel boots just fine on the
    Raspberry Pi 4 using our common configuration.

--- nixos/modules/installer/cd-dvd/sd-image-aarch64.nix
+++ nixos/modules/installer/cd-dvd/sd-image-aarch64.nix
@@ -18,7 +18,7 @@
   # - ttyS0: for Tegra (Jetson TX1)
   # - ttyAMA0: for QEMU's -machine virt
   # Also increase the amount of CMA to ensure the virtual console on the RPi3 works.
-  boot.kernelParams = ["cma=32M" "console=ttyS0,115200n8" "console=ttyAMA0,115200n8" "console=tty0"];
+  boot.kernelParams = ["console=ttyS0,115200n8" "console=ttyAMA0,115200n8" "console=tty0"];
 
   boot.initrd.availableKernelModules = [
     # Allows early (earlier) modesetting for the Raspberry Pi
